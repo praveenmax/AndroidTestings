52e6907146399014949c1fe5e95582b10156250b
Praveen <com.java.praveen@gmail.com>
2017-07-07 19:41:10 +0530

    Test commit from android studio

--- app/src/main/java/com/example/praveenmax/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/praveenmax/myfirstapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
