5abf96eb14c8916a045512ae9449356843e71211
Praveen <com.java.praveen@gmail.com>
2017-07-14 20:10:51 +0530

    Added seperate Activity class for MainActivitySwitcher

--- app/src/main/java/com/example/praveenmax/myfirstapp/activityswitcher/DisplayMessageActivity.java
+++ app/src/main/java/com/example/praveenmax/myfirstapp/activityswitcher/DisplayMessageActivity.java
@@ -1,19 +1,21 @@
-package com.example.praveenmax.myfirstapp;
+package com.example.praveenmax.myfirstapp.activityswitcher;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.example.praveenmax.myfirstapp.R;
+
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        setContentView(R.layout.my_activity_display_message);
 
         Intent intent = getIntent();
-        String receivedMessage = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String receivedMessage = intent.getStringExtra(MainActivitySwitcher.EXTRA_MESSAGE);
 
         EditText editText = (EditText)findViewById(R.id.editText5);
         editText.setText(receivedMessage);
---
+++ app/src/main/java/com/example/praveenmax/myfirstapp/activityswitcher/MainActivitySwitcher.java
@@ -0,0 +1,34 @@
+package com.example.praveenmax.myfirstapp.activityswitcher;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.praveenmax.myfirstapp.R;
+
+
+public class MainActivitySwitcher extends AppCompatActivity {
+
+
+    public static final String EXTRA_MESSAGE = "praveenmax.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activityswitcher);
+    }
+
+    //Called when the user taps send button
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText et= (EditText)findViewById(R.id.editText);
+        String message = et.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
